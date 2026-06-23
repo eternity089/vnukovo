043f2cf62a2ba7cfde4b4369e8d1eb8258ead44a
eternity089 <anxietlyeternity@gmail.com>
2026-06-23 12:16:41 +0300

    ready for docker on vps

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,15 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-
-  base: "/",
-
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://backend:8000",
-        changeOrigin: true,
-      }
-    }
-  }
+  base: "./"
 })
